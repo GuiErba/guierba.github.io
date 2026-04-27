5ad7a5d0ea9ea1cb0d0178a20d46ec7999ac4730
GuiiHenriq <guilhermeerba@hotmail.com>
2026-04-27 10:06:00 -0300

    chore: remove old project structure

--- app/components/footer/index.tsx
+++
@@ -1,9 +0,0 @@
-export const Footer = () => {
-  return (
-    <footer className="container py-10 text-center border-t border-zinc-800">
-      <p className="text-zinc-400 text-sm">
-        © 2026 Guilherme Erba. All rights reserved.
-      </p>
-    </footer>
-  )
-}
--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['media.graphassets.com', 'sa-east-1.graphassets.com'],
-  },
-}
-
-module.exports = nextConfig
